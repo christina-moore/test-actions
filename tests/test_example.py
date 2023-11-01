965bea295da5d4a9a31424886271426cb380a76c
ChristinaMoore <christina.moore@quickrelease.co.uk>
2023-11-01 20:25:51 +0000

    build: add first test github action

---
+++ tests/test_example.py
@@ -0,0 +1,15 @@
+import sys
+import os
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from src.example import add, subtract
+
+
+def test_add():
+    assert add(1, 2) == 3
+
+
+def test_subtract():
+    assert subtract(5, 2) == 3
